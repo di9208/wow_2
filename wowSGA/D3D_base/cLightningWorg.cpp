6ac820da41303c4e52a9f4ce54f5f4ff472f2495
odd1258 <odd1258@naver.com>
2018-02-14 17:23:55 +0900

    몬스터 팅기려나?
    ㅇㅇ

--- wowSGA/D3D_base/cLightningWorg.cpp
+++ wowSGA/D3D_base/cLightningWorg.cpp
@@ -216,15 +216,14 @@ void cLightningWorg::Update(iMap* map) {
 	if (m_vecSkinnedMesh.size() != 0)
 	{
 		for (size_t i = 0; i < m_vecSkinnedMesh.size(); i++) {
-			//몬스터 죽음
-			if (m_vecSkinnedMesh[i].t.HP <= 0) m_vecSkinnedMesh[i].ENUM_MONSTER = MONSTER_STATUS::MONSTER_DEATH;
-
-
 			matUpdate(i, map);
 			m_vecSkinnedMesh[i].m->Update();
 			m_vecSkinnedMesh[i].Particle->update(3.0f);
 			m_vecSkinnedMesh[i].MonsterOBB->Update(&m_vecSkinnedMesh[i].matRT);
 			MonsterAI(i);						//몬스터의 패턴, 스킬
+
+			//몬스터 죽음
+			if (m_vecSkinnedMesh[i].t.HP <= 0) m_vecSkinnedMesh[i].ENUM_MONSTER = MONSTER_STATUS::MONSTER_DEATH;
 			MonsterStatus(i); 					//몬스터 상태, 애니메이션
 		}
 	}
